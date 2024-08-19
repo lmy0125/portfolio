17f1ee1fcd7bc0960185ba7be5924d20166425fd
lmy0125 <50187300+lmy0125@users.noreply.github.com>
2024-08-18 22:23:43 -0700

    removed resume nav and page

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -7,36 +7,37 @@ import 'react-pdf/dist/Page/TextLayer.css';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 
 // Set the worker source for react-pdf
-pdfjs.GlobalWorkerOptions.workerSrc = new URL('pdfjs-dist/build/pdf.worker.min.mjs', import.meta.url).toString();
+// pdfjs.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.worker.min.js';
+// pdfjs.GlobalWorkerOptions.workerSrc = new URL('pdfjs-dist/build/pdf.worker.min.mjs', import.meta.url).toString();
 
 const PDFDisplayPage = () => {
-	const [width, setWidth] = useState<number>(600);
+	// const [width, setWidth] = useState<number>(600);
 
-	useEffect(() => {
-		const updateWidth = () => {
-			const containerWidth = document.getElementById('pdf-container')!.clientWidth;
-			setWidth(containerWidth - 20); // Subtract padding
-		};
+	// useEffect(() => {
+	// 	const updateWidth = () => {
+	// 		const containerWidth = document.getElementById('pdf-container')!.clientWidth;
+	// 		setWidth(containerWidth - 20); // Subtract padding
+	// 	};
 
-		updateWidth();
-		window.addEventListener('resize', updateWidth);
-		return () => window.removeEventListener('resize', updateWidth);
-	}, []);
+	// 	updateWidth();
+	// 	window.addEventListener('resize', updateWidth);
+	// 	return () => window.removeEventListener('resize', updateWidth);
+	// }, []);
 
-	const pdfUrl = '/resume.pdf';
+	// const pdfUrl = '/resume.pdf';
 
-	const handleDownload = () => {
-		const link = document.createElement('a');
-		link.href = pdfUrl;
-		link.download = 'David SWE Resume.pdf'; // You can set any name here
-		document.body.appendChild(link);
-		link.click();
-		document.body.removeChild(link);
-	};
+	// const handleDownload = () => {
+	// 	const link = document.createElement('a');
+	// 	link.href = pdfUrl;
+	// 	link.download = 'David SWE Resume.pdf'; // You can set any name here
+	// 	document.body.appendChild(link);
+	// 	link.click();
+	// 	document.body.removeChild(link);
+	// };
 
 	return (
 		<div className="relative flex flex-col items-center min-h-[100dvh] p-4 w-full">
-			<div className="p-6 max-w-3xl w-full">
+			{/* <div className="p-6 max-w-3xl w-full">
 				<div className="flex justify-between items-center mb-4">
 					<h1 className="text-2xl sm:text-3xl xl:text-4xl font-bold tracking-tight">Resume</h1>
 					<Button onClick={handleDownload} className="flex items-center gap-2">
@@ -46,13 +47,10 @@ const PDFDisplayPage = () => {
 				</div>
 				<div id="pdf-container" className="overflow-hidden">
 					<Document file={pdfUrl} className="flex justify-center">
-						<Page
-							pageNumber={1}
-							width={width}
-						/>
+						<Page pageNumber={1} width={width} />
 					</Document>
 				</div>
-			</div>
+			</div> */}
 		</div>
 	);
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { CodeIcon, HomeIcon, NotebookIcon, PencilLine } from 'lucide-react';
-import * as React from 'react';
+import { useEffect } from 'react';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
 import { Icons } from '@/components/icons';
@@ -11,35 +11,35 @@ const NavBar = () => {
 
 	const navItems = [
 		{ label: 'Home', href: '/' },
-		// { label: 'Projects', href: '/projects' },
+		{ label: 'Projects', href: '#projects' },
 		// { label: 'Blogs', href: '/blogs' },
-		{ label: 'Resume', href: '/resume' },
+		// { label: 'Resume', href: '/resume' },
 	];
 
 	return (
 		// <nav className="bg-white shadow-sm">
-			<nav className="relative max-w-7xl mx-auto flex justify-center h-16">
-				<div className="flex items-center space-x-8">
-					{/* <div className="flex-shrink-0 flex items-center">
+		<nav className="relative max-w-7xl mx-auto flex justify-center h-16">
+			<div className="flex items-center space-x-8">
+				{/* <div className="flex-shrink-0 flex items-center">
               <span className="text-xl font-bold text-gray-800">Logo</span>
             </div> */}
-					<div className="sm:flex space-x-8">
-						{navItems.map((item) => (
-							<Link
-								key={item.href}
-								href={item.href}
-								className={cn(
-									'inline-flex items-center pt-1 border-b-2 text-sm font-medium',
-									pathname === item.href
-										? 'border-[#408AF8] text-gray-900'
-										: 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'
-								)}>
-								{item.label}
-							</Link>
-						))}
-					</div>
+				<div className="sm:flex space-x-8">
+					{navItems.map((item) => (
+						<Link
+							key={item.href}
+							href={item.href}
+							className={cn(
+								'inline-flex items-center pt-1 border-b-2 text-sm font-medium',
+								pathname === item.href
+									? 'border-[#408AF8] text-gray-900'
+									: 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'
+							)}>
+							{item.label}
+						</Link>
+					))}
 				</div>
-			</nav>
+			</div>
+		</nav>
 		// </nav>
 	);
 };
