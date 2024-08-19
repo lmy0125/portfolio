aca4f1109c338d15b0dab5acffc2de2725762d1b
lmy0125 <50187300+lmy0125@users.noreply.github.com>
2024-08-18 23:02:13 -0700

    changed meteors top

--- src/components/magicui/meteors.tsx
+++ src/components/magicui/meteors.tsx
@@ -14,7 +14,7 @@ export const Meteors = ({ number = 20 }: MeteorsProps) => {
 
   useEffect(() => {
     const styles = [...new Array(number)].map(() => ({
-      top: 0,
+      top: -5,
       left: Math.floor(Math.random() * window.innerWidth / 2) + "px",
       animationDelay: Math.random() * 1 + 0.2 + "s",
       animationDuration: Math.floor(Math.random() * 8 + 2) + "s",
