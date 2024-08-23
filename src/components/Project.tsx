e485dd982994a12c5b686cc2b7b1f43e325c4f00
lmy0125 <50187300+lmy0125@users.noreply.github.com>
2024-08-22 22:50:47 -0700

    improved style for project page

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -11,7 +11,9 @@ export const SingleProduct = ({ project }: { project: Project }) => {
 	return (
 		<div className="bt-2 pb-20">
 			<div className="flex lg:flex-row justify-between items-center flex-col mb-8">
-				<h1 className="text-2xl sm:text-3xl xl:text-4xl font-bold tracking-tight pb-1 text-neutral-700">{project.title}</h1>
+				<h1 className="text-2xl sm:text-3xl xl:text-4xl font-bold tracking-tight pb-1 text-neutral-700">
+					{project.title}
+				</h1>
 				<div className="mt-3 sm:mt-0">
 					{project.github && (
 						<Link
@@ -34,9 +36,9 @@ export const SingleProduct = ({ project }: { project: Project }) => {
 								viewBox="0 0 24 24"
 								fill="none"
 								stroke="currentColor"
-								stroke-width="2"
-								stroke-linecap="round"
-								stroke-linejoin="round"
+								strokeWidth="2"
+								strokeLinecap="round"
+								strokeLinejoin="round"
 								className="w-3.5 h-3.5 group-hover:translate-x-0.5 transition-transform">
 								<path d="M5 12l14 0"></path>
 								<path d="M13 18l6 -6"></path>
@@ -62,19 +64,22 @@ export const SingleProduct = ({ project }: { project: Project }) => {
 				className="relative">
 				{project.video ? (
 					<video
-						src={project.video}
 						autoPlay
 						loop
+						controls
 						muted
 						playsInline
-						className="pointer-events-none mx-auto w-11/12 object-cover object-top" // needed because random black line at bottom of video
-					/>
+						className="z-10 mx-auto w-11/12 object-cover object-top" // needed because random black line at bottom of video
+					>
+						<source src={project.video} type="video/mp4" />
+						Your browser does not support the video.
+					</video>
 				) : (
 					<div className="relative h-64 md:h-96 overflow-hidden w-11/12 mx-auto">
 						<Image src={project.image!} alt="thumbnail" fill className="object-cover object-top" />
 					</div>
 				)}
-				<div className="absolute bottom-0 bg-white h-40 w-full [mask-image:linear-gradient(to_bottom,transparent,white)]" />
+				{/* <div className="absolute bottom-0 bg-white h-40 w-full [mask-image:linear-gradient(to_bottom,transparent,white)]" /> */}
 			</motion.div>
 			{/* <div className="flex flex-row justify-center my-8 flex-wrap">
 				{project.images?.map((image, idx) => (
@@ -89,8 +94,8 @@ export const SingleProduct = ({ project }: { project: Project }) => {
 					</button>
 				))}
 			</div> */}
-			<div className="max-w-2xl mx-auto">
-				<div className="flex space-x-2 md:mb-1 mt-2 md:mt-0">
+			<div className="max-w-2xl mx-auto mt-5">
+				<div className="flex flex-wrap gap-2 md:mb-1 mt-2 md:mt-0">
 					{project.stack?.map((stack: string) => (
 						<Badge className="px-1 py-0 text-xs" variant="secondary" key={stack}>
 							{stack}
@@ -98,7 +103,7 @@ export const SingleProduct = ({ project }: { project: Project }) => {
 					))}
 				</div>
 				<div>
-					<p className="lg:text-lg max-w-3xl text-neutral-500 mt-4">{project.description}</p>
+					<div className="lg:text-lg max-w-3xl text-neutral-500 mt-4">{project.description}</div>
 				</div>
 				<div className="prose lg:prose-lg max-w-none prose-stone text-neutral-900">{project?.content}</div>
 			</div>
